ed1d0d652ca1bbf229d9a26d5902a312a6743541
Artemas <artemasseng@gmail.com>
2017-11-15 01:25:38 +0000

    Amended ShoppingServiceTest

--- src/test/java/com/simpleapp/Service/StoreDashBoardServiceTest.java
+++ src/test/java/com/simpleapp/Service/StoreDashBoardServiceTest.java
@@ -10,6 +10,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
@@ -23,64 +24,17 @@ import static org.junit.Assert.*;
 /**
  * Created by amuzanenhamo on 17/05/2017.
  */
+@DataJpaTest
 @RunWith(SpringRunner.class)
-@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class StoreDashBoardServiceTest {
 
-    @TestConfiguration
-    static class ShoppingAppServiceImplTestContextConfiguration {
-
-        @Bean
-        public ShoppingAppService shoppingAppServiceImpl() {
-            return new ShoppingAppServiceImpl();
-        }
-
-    }
-
     @Autowired
-    private ShoppingAppService shoppingAppService;
-
-    @MockBean
     private ShoppingRepository shoppingRepository;
 
     @Before
-    public void setUp(){
-
-        Item fruit = new Item("orange", "Orange", "Citric Fruit", "Fruit");
-        Item shoe = new Item("jordan5", "Nike Air Jordan V5", "Basketball shoes", "Footwear");
-
-        List<Item> list = new ArrayList<>();
-        list.add(fruit);
-        list.add(shoe);
-
-        int listSize = list.size();
-
-        Mockito.when(shoppingRepository.findOne(fruit.getId())).thenReturn(fruit);
-        Mockito.when(shoppingRepository.findOne(shoe.getId())).thenReturn(shoe);
-        Mockito.when(shoppingRepository.count()).thenReturn(2L);
-
-        shoppingRepository.save(list.get(0));
-        shoppingRepository.save(list.get(1));
-    }
-
-    @Test
-    public void whenANullListIsPassedInThenGETBasketPassedWillNotAddIt() throws Exception{
-
-        Item item = null;
-
-        shoppingAppService.addItemToBasket(item);
-        assertTrue(shoppingAppService.getBasket().size() == 0);
-    }
-
-
-    @Test
-    public void whenAnEmptyItemPassedThenDoNotAddToList() throws Exception{
-
-        Item item = new Item("", "", "", "");
-
-        shoppingAppService.addItemToBasket(item);
-
-        assertEquals(0, shoppingAppService.getBasket().size());
+    public void before(){
+        Item item = new Item("tangarine", "Tangarine", "Similar to an Orange", "Citric");
+        shoppingRepository.save(item);
     }
 
     @Test
@@ -88,42 +42,30 @@ public class StoreDashBoardServiceTest {
 
         Item item = new Item("apples", "Apples", "another fruit", "Fruit");
 
-        shoppingAppService.addItemToBasket(item);
+        shoppingRepository.save(item);
 
         assertEquals(2, shoppingRepository.count());
     }
 
-    @Test
-    public void whenANullIdValueIsPassedInThenGETBasketByIdWillNotDoReturnAnything() throws Exception {
-
-        assertEquals("", shoppingAppService.getBasketById(null).getId());
-
-        assertNotNull(shoppingAppService.getBasketById(null));
-
-    }
 
     @Test
-    public void whenAnEmptyIdValueIsPassedInThenGETBasketByIdWillNotDoReturnAnything() throws Exception {
+    public void whenAValidIdIsPassedInThenGETBasketByIdWillReturnAnItem() throws Exception {
 
-        assertEquals("", shoppingAppService.getBasketById("").getId());
+        Item item = new Item("orange", "Orange", "Fruit", "Fruit");
+        shoppingRepository.save(item);
 
-        assertNotNull(shoppingAppService.getBasketById(""));
+        assertEquals("Orange", shoppingRepository.findOne("orange").getName());
     }
-
-//    @Test
-//    public void whenAValidIdIsPassedInThenGETBasketByIdWillReturnAnItem() throws Exception {
-//
-//        assertEquals("Orange", shoppingAppService.getBasketById("orange").getName());
-//    }
 
     @Test
     public void whenAListItemIsDeletedFromTheListThenReturnTheListMinusTheItem() throws Exception {
 
-        String id = "orange";
+        Item item = new Item("orange", "Orange", "Fruit", "Fruit");
+        shoppingRepository.save(item);
 
-        shoppingAppService.deleteItem(id);
+        shoppingRepository.delete("tangarine");
 
-        assertEquals(2, shoppingRepository.count());
+        assertEquals(1, shoppingRepository.count());
 
     }
 
